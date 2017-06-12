148633d31e034721f567f8443212739ee7cb45b7
Craig N. Caroon <craig@caroon.org>
2017-06-12 19:09:43 -0400

    Updated ns_proxy.js to send HTTP requests to ns_server.py instead of spawning process instances of ns_cli.py

--- neo_stairs/ns_proxy.js
+++ neo_stairs/ns_proxy.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
+const http  = require('http');
 const Blynk = require("blynk-library");
-const spawn = require("child_process").spawn;
 
 var stairsServer = new Blynk.Blynk('6d8a00fee2ac4da59e7c87721609286b');
-var button       = new stairsServer.VirtualPin(0);
 var rgb          = new stairsServer.VirtualPin(1);
-
-const COMMAND = './neo_stairs.py';
 
 // Maps "a" to a number between 0-255 from 0-1023
 function map(x) {
@@ -18,26 +15,19 @@ rgb.on('write', (params) => {
     var red   = Math.round(map(params[0]));
     var green = Math.round(map(params[1]));
     var blue  = Math.round(map(params[2]));
-    // console.log(`Red: [${params[0]}][${red}]`);
-    // console.log(`Green: [${params[1]}][${green}]`);
-    // console.log(`Blue: [${params[2]}][${blue}]`);
 
-    var cmd = spawn("sudo", [`${COMMAND}`, "rgb", `--red=${red}`, `--green=${green}`, `--blue=${blue}`]);
+    http.get(`http://localhost:8000/set_color?red=${red};green=${green};blue=${blue}`, (res) => {
+        if (res.statusCode !== 200) {
+            console.log(`GET Error [${res.statusCode}]`);
+            res.resume();
+            return;
+        }
 
-    cmd.stderr.on("data", (data) => {
-        console.log(`${data}`);
-    });
-
-    cmd.on("error", (err) => {
-        console.log(`${err}`);
+        res.on('end', () => {
+            console.log("Successfully set the color");
+        }).on('error', (err) => {
+            console.log(`Error: ${e.message}`);
+        });
     });
 })
 
-
-button.on('write', function(params) {
-    var cmd = spawn("sudo", [`${COMMAND}`, "random"]);
-
-    cmd.on("error", (err) => {
-        console.log(`${err}`);
-    });
-});
